7e4eae744370e9a46abede3d76fc0b48b615b1ec
koki-develop <kou.pg.0131@gmail.com>
2023-04-15 19:59:39 +0900

    render grass

--- cmd/root.go
+++ cmd/root.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/charmbracelet/lipgloss"
 	"github.com/cli/go-gh"
@@ -18,6 +19,16 @@ const (
 	contributionLevelThirdQuartile  contributionLevel = "THIRD_QUARTILE"
 	contributionLevelFourthQuartile contributionLevel = "FOURTH_QUARTILE"
 )
+
+var themes = map[string]map[contributionLevel]string{
+	"dark-default": {
+		contributionLevelNone:           "#161B22",
+		contributionLevelFirstQuartile:  "#0E4429",
+		contributionLevelSecondQuartile: "#006D32",
+		contributionLevelThirdQuartile:  "#26A641",
+		contributionLevelFourthQuartile: "#39D353",
+	},
+}
 
 type viewerQuery struct {
 	Viewer struct {
@@ -46,7 +57,19 @@ var rootCmd = &cobra.Command{
 			return err
 		}
 
-		fmt.Printf("%#v", query)
+		theme := themes["dark-default"] // TODO: from flag
+
+		for i := 0; i < 7; i++ {
+			chars := make([]string, len(query.Viewer.ContributionsCollection.ContributionCalendar.Weeks))
+			for j, w := range query.Viewer.ContributionsCollection.ContributionCalendar.Weeks {
+				d := w.ContributionDays[i]
+				c := lipgloss.Color(theme[d.ContributionLevel])
+				style := lipgloss.NewStyle().Foreground(c)
+				chars[j] = style.Render("■")
+			}
+			fmt.Print(strings.Join(chars, " "))
+			fmt.Print("\n")
+		}
 		return nil
 	},
 }
