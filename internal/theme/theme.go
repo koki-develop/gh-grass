42623c6e499136548b9d01d206cad582c778e08f
koki-develop <kou.pg.0131@gmail.com>
2023-05-03 06:26:59 +0900

    implement theme.Register

--- internal/theme/theme.go
+++ internal/theme/theme.go
@@ -2,34 +2,37 @@ package theme
 
 import "github.com/koki-develop/gh-grass/internal/github"
 
+var (
+	_ = Register("dark", "#161B22", "#0E4429", "#006D32", "#26A641", "#39D353")
+	_ = Register("light", "#EBEDF0", "#9BE9A8", "#40C463", "#30A14E", "#216E39")
+)
+
 type Theme map[github.ContributionLevel]string
 
-var themes = map[string]Theme{
-	"dark": {
-		github.ContributionLevelNone:           "#161B22",
-		github.ContributionLevelFirstQuartile:  "#0E4429",
-		github.ContributionLevelSecondQuartile: "#006D32",
-		github.ContributionLevelThirdQuartile:  "#26A641",
-		github.ContributionLevelFourthQuartile: "#39D353",
-	},
-	"light": {
-		github.ContributionLevelNone:           "#EBEDF0",
-		github.ContributionLevelFirstQuartile:  "#9BE9A8",
-		github.ContributionLevelSecondQuartile: "#40C463",
-		github.ContributionLevelThirdQuartile:  "#30A14E",
-		github.ContributionLevelFourthQuartile: "#216E39",
-	},
+var registry = map[string]Theme{}
+
+func Register(name, none, first, second, third, fourth string) Theme {
+	t := Theme{
+		github.ContributionLevelNone:           none,
+		github.ContributionLevelFirstQuartile:  first,
+		github.ContributionLevelSecondQuartile: second,
+		github.ContributionLevelThirdQuartile:  third,
+		github.ContributionLevelFourthQuartile: fourth,
+	}
+	registry[name] = t
+
+	return t
 }
 
 func Get(name string) (Theme, bool) {
-	t, ok := themes[name]
+	t, ok := registry[name]
 	return t, ok
 }
 
 func List() []string {
 	ts := []string{}
 
-	for t := range themes {
+	for t := range registry {
 		ts = append(ts, t)
 	}
 
